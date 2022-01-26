8a38864631668195d896de39ca3bb851e8409707
lexfer98 <al386478@uji.es>
2022-01-26 09:42:17 +0100

    add functions.h

--- functions.h
+++ functions.h
@@ -3,22 +3,18 @@
 
 
 
-/** @return random value from the normal distribution N(170,15). */
-double random_value(); 
 
-/** The function prints a matrix of random numbers in a file
-  @param FILE_NAME  file name :-)
-  @param ROWS       number of rows
-  @param COLS       number of columns
- */
 void  print_matrix (const std::string & FILE_NAME, const int ROWS, const int COLS);
 
-/** The function reads a matrix of numbers from a text file.
-  @param FILE_NAME file name 
-  @return a matrix of doubles*/
-
-
-
 std::vector<std::vector<double>>  read_matrix(const std::string & FILE_NAME);
+
+std::vector<std::vector<double>> multiplymatrices(std::vector<std::vector<double>> m1, std::vector<std::vector<double>> m2);
+
+std::vector<std::vector<double>> additionmatrices(std::vector<std::vector<double>> m1, std::vector<std::vector<double>> m2);
+
+std::vector<std::vector<double>> subtractionmatrices(std::vector<std::vector<double>> m1, std::vector<std::vector<double>> m2);
+
+std::vector<std::vector<double>> transposematrix(std::vector<std::vector<double>> m1);
+
 
 #endif
